9b58d4c3f533983217c10dbe41a12429b982d26a
Anuj <anujsinha789@gmail.com>
2022-02-01 12:04:27 +0530

    UI for login screen

--- bank-management-system/src/screens/login/index.js
+++ bank-management-system/src/screens/login/index.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Checkbox } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import style from "./style.module.css";
+import LoginIcon from "@mui/icons-material/Login";
 
 function Login() {
+  const [checked, setChecked] = React.useState(false);
   return (
     <div className={style.root}>
       <div className={style.login_form_container}>
         <div className={style.avatar_container}>
-          <div className={style.login_avatar}></div>
+          <div className={style.login_avatar}>
+            <h2 className={style.login_text}>Login</h2>
+            <LoginIcon style={{ height: "5vh", width: "5vw" }} />
+          </div>
         </div>
         <div className={style.login_form}>
           <div className={style.email_input_container}>
@@ -14,6 +21,7 @@ function Login() {
               className={style.email_input}
               name="email"
               placeholder="Email"
+              autoComplete="off"
               type="Email"
               required="true"
             />
@@ -23,15 +31,31 @@ function Login() {
               className={style.password_input}
               name="password"
               placeholder="Password"
-              autoComplete="off"
-              type="Email"
+              type="password"
               required="true"
             />
           </div>
           <div className={style.btn_container}>
+            <div className={style.rememeberMeCheckbox}>
+              <Checkbox
+                checked={checked}
+                onChange={() => {
+                  setChecked(() => !checked);
+                }}
+                inputProps={{ "aria-label": "Remember Me" }}
+                sx={{ "& .MuiSvgIcon-root": { fontSize: 28 } }}
+                style={{ color: "black" }}
+              />
+              Remember Me
+            </div>
             <button type="button" class="btn btn-success">
               Login
             </button>
+          </div>
+          <div className={style.footer_container}>
+            <span className={style.footer_text}>
+              New Member ? <Link to="/signup">Sign-up</Link>
+            </span>
           </div>
         </div>
       </div>
